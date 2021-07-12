a08453adb2133ad7c0d99100d406e75cd9960193
give57ao <give57ao@gmail.com>
2021-07-12 23:16:46 +0900

    REST API구축 (불러오기는 실패한 상태) & 파일변경 등

---
+++ client/src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import Detail from './components/Detail';
+import './App.css';
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+import { mergeClasses } from '@material-ui/styles';
+
+const styles = theme => (
+  {
+    root: {
+      width: '100%',
+      //marginTop : theme.spacing.unit * 3,
+      overflowX: "auto"
+    },
+    table:{
+      minWidth : 1080
+    }
+  }
+)
+class App extends Component{
+  state ={
+    details : ""
+  }
+  componentDidMount(){
+    this.callApi()
+    .then(res => this.setState({details : res}))
+    .catch(err => console.log(err));
+  }
+  callApi = async () => {
+    const response = await fetch('/api/details'); //http://localhost:5000/api/details
+    const body = await response.json();
+    return body;
+  } 
+ 
+    render(){
+      const {classes} = this.props; 
+    return(
+      <Paper className={classes.root}>
+   
+       <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>작성자</TableCell>
+              <TableCell>제목</TableCell>
+              <TableCell>날짜</TableCell>
+          </TableRow>
+        </TableHead>
+      
+      <TableBody>
+      {this.state.details ? this.state.details.map(d => { return ( <Detail key = {d.id} id = {d.id} image = {d.image} name = {d.name} title = {d.title} date = {d.date}/>)}) : ""} 
+      </TableBody>
+      </Table>
+              
+          
+      </Paper>  
+     
+    );
+  } 
+  }
+
+
+export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -6,7 +6,34 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!`'});
+app.get('/api/details', (req, res) => {
+    res.send(
+        [
+            {
+                'id' :1,
+                'image': 'https://placeimg.com/64/64/1',
+                'name'  : '강윤묵 / 화정신문',
+                'title': '테스트',
+                'content': '테스트입니다',
+                'date': '2021-07-09'
+              },
+              {
+                'id' :2,
+                'image': 'https://placeimg.com/64/64/2',
+                'name'  : '김윤수 / 대구신문',
+                'title': '테스트2', 
+                'content': '테스트2입니다',
+                'date': '2021-07-09'
+              },
+              {
+                'id' :3,
+                'image': 'https://placeimg.com/64/64/3',
+                'name'  : '김재백 / 대구신문',
+                'title': '테스트3',
+                'content': '테스트3입니다',
+                'date': '2021-07-09'
+              }
+        ]
+    );
 })
 app.listen(port, () => console.log(`Listening on port ${port}`));
